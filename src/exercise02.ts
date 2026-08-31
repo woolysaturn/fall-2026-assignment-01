15a157670710b15df282a676effb648af1ed0241
Raina <nyfr2283@gmail.com>
2026-08-31 06:48:32 -0400

    Complete exercises 1-3

--- src/exercise02.ts
+++ src/exercise02.ts
@@ -1,3 +1,40 @@
+// custom error class
+export class InvalidNucleotideError extends Error{
+  constructor(invalidChar: string){
+    //passing messgae to base error class
+    super('Invalid DNA nucleotide found: ${invalidChar}');
+    //error name
+    this.name = "InvalidNucleotideError";
+  }
+}
+// this makes the custom error to be thrown
+
 export function transcribeDNA(dna: string): string {
-  return '';
+
+  let result = "";
+
+  for (const charr of dna){
+    if(charr == 'A'){
+      result += 'U';
+    }
+
+    else if(charr == 'T'){
+      result += 'A';
+    }
+
+    else if(charr == 'C'){
+      result += 'G';
+    }
+
+    else if(charr == 'G'){
+      result += 'C';
+    }
+
+    else {
+      throw new InvalidNucleotideError('Invalid Dna nucleotide: ${charr}');
+    }
+  }
+
+  return result;
+
 }
--- src/exercise03.ts
+++ src/exercise03.ts
@@ -1,5 +1,13 @@
 export function getInventoryValue(
   inventory: Array<[string, number, number]>,
 ): number {
-  return 0;
+
+//item name, how many, price
+//run a long calculator to check and add
+  return inventory.reduce((total, [_name, quantity, price]) => {
+    if(quantity > 5){
+      return total + quantity * price;
+    }  
+    return total;
+  }, 0)
 }
