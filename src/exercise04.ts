ab2e82e7a3a10307110b1b5928e4fe9945819251
Raina <nyfr2283@gmail.com>
2026-08-31 08:21:19 -0400

    Complete exercise 4-5

--- src/exercise04.ts
+++ src/exercise04.ts
@@ -1,11 +1,32 @@
-export type Circle = {};
+export type Circle = {
+  kind: 'circle';
+  radius: number;
+};
 
-export type Rectangle = {};
+export type Rectangle = {
+  kind: 'rectangle';
+  width: number;
+  height: number;
+};
 
-export type Square = {};
+export type Square = {
+  kind: 'square';
+  sideLength: number;
+};
 
 export type Shape = Circle | Rectangle | Square;
 
 export function calculateArea(shape: Shape): number {
-  return 0;
+  // circle area is pi*raduis^2
+  if(shape.kind == 'circle'){
+    return Math.PI *shape.radius ** 2;
+  }
+  //rectangle area w*l
+  else if(shape.kind == 'rectangle'){
+    return shape.width * shape.height;
+  }
+  //square sidelegnth^2
+  else{
+    return shape.sideLength **2;
+  }
 }
--- src/exercise05.ts
+++ src/exercise05.ts
@@ -1,9 +1,20 @@
-export type NetworkConfig = {};
+export type NetworkConfig = {
+  serverUrl: string;
+  port: number;
+};
 
-export type EnvironmentConfig = {};
+export type EnvironmentConfig = {
+  environment: 'dev' | 'prod';
+  timeout: number;
+};
 
 export type AppConfig = NetworkConfig & EnvironmentConfig;
 
 export function initializeConfig(userOverrides: Partial<AppConfig>): AppConfig {
-  return {};
+
+  //creating variable assigned to defualt configuration folling the app config type rules
+  const defualtConfig: AppConfig = {serverUrl: "http://localhost", port: 8080, environment: "dev", timeout: 3000,};
+
+  return{...defualtConfig, ...userOverrides,}
+  
 }
