ce6731e6ecca175d384257fee249c9284e1f1d44
Raina <nyfr2283@gmail.com>
2026-09-01 07:16:00 -0400

    Complete exercise 7

--- src/exercise07.ts
+++ src/exercise07.ts
@@ -1,3 +1,31 @@
+import fs from 'fs';
+import path from 'path';
+
+  export type SubjectScores = {[subject: string]: number;}
+  export type Gradebook = {[studentName: string]: SubjectScores;}
+
+
 export function calculateSubjectAverage(subject: string): number {
-  return 0;
+
+  const filePath = path.join(process.cwd(), 'data', 'gradebook.json');
+  const fileData = fs.readFileSync(filePath, 'utf-8');
+  const gradebook: Gradebook = JSON.parse(fileData);
+
+  let totalScore = 0;
+  let studentCount = 0;
+
+  for(const student in gradebook){
+    const scores = gradebook[student];
+    if(subject in scores){
+      totalScore += scores[subject];
+      studentCount += 1;
+    }
+  }
+  if(studentCount == 0){
+    return 0;
+  }
+  else{
+    return totalScore / studentCount;
+  }
+
 }
