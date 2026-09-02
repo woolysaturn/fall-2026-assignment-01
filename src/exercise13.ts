d3e0a6eb6cd82ce36e4f8ff552c8aa0a00ace75f
Raina <nyfr2283@gmail.com>
2026-09-02 18:56:11 -0400

    Complete exercise 13-14

--- src/exercise13.ts
+++ src/exercise13.ts
@@ -1,5 +1,21 @@
-export type TodoItem = {};
+export type TodoItem = {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+};
 
 export async function fetchTodoSafe(todoId: number): Promise<TodoItem | null> {
-  return null;
+  const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${todoId}`);
+  try{
+    if(!response.ok){
+      return null;
+    }
+
+    const todo: TodoItem = await response.json();
+    return todo;
+  }
+  catch(error){
+    return null;
+  }
 }
--- src/exercise14.ts
+++ src/exercise14.ts
@@ -1,5 +1,14 @@
-export type PostItem = {};
+export type PostItem = {
+  id: number;
+  title: string;
+  body: string;
+};
 
 export async function fetchPostBatch(postIds: number[]): Promise<PostItem[]> {
-  return [];
+  const fetchPromises = postIds.map(async (id) => {  
+    const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+    const post: PostItem = await response.json();
+    return post;
+  })
+  return await Promise.all(fetchPromises);
 }
