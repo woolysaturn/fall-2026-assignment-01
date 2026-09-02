2f30396a007ab8ffaec1781a983d183395791258
Raina <nyfr2283@gmail.com>
2026-09-02 18:45:05 -0400

    Complete exercise 11-12

--- src/exercise11.ts
+++ src/exercise11.ts
@@ -1,4 +1,9 @@
+import {promises as fs} from 'fs';
+
 export async function logStatusToFile(
   filePath: string,
   statusMessage: string,
-): Promise<void> {}
+): Promise<void> {
+  const log = `${statusMessage} - ${new Date().toISOString()}\n`;
+  await fs.appendFile(filePath, log);
+}
